6a38712bd8b55c591044a2ea64decec34300e8ba
Juliana Martins <julianaalvesmartins1@gmail.com>
2025-11-20 19:24:40 -0300

    Ajustes gerais em formulários, views e layout

--- apps/colaboradores/views.py
+++ apps/colaboradores/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from django.contrib import messages
 from .models import Colaborador
 
 class HomeView(TemplateView):
@@ -20,13 +21,26 @@ class ColaboradorCreate(CreateView):
     success_url = reverse_lazy('colaboradores:lista')
     template_name = 'colaboradores/form.html'
 
+    def form_valid(self, form):
+        messages.success(self.request, "✔ Colaborador cadastrado com sucesso!")
+        return super().form_valid(form)
+
 class ColaboradorUpdate(UpdateView):
     model = Colaborador
     fields = ['nome', 'cpf', 'setor', 'foto_colaborador']
     success_url = reverse_lazy('colaboradores:lista')
     template_name = 'colaboradores/form.html'
 
+    def form_valid(self, form):
+        messages.success(self.request, "✔ Colaborador atualizado com sucesso!")
+        return super().form_valid(form)
+
 class ColaboradorDelete(DeleteView):
     model = Colaborador
     success_url = reverse_lazy('colaboradores:lista')
     template_name = 'colaboradores/confirma_exclusao.html'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "✔ Colaborador excluído com sucesso!")
+        return super().delete(request, *args, **kwargs)
+
